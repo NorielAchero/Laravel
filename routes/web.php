01862679ec4c43a910072f703ec798c7bff5c779
Noriel Achero <acheronorielr@gmail.com>
2024-10-23 12:09:02 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get();
+// Route::pos();
+// Route::put();
+// Route::patch();
+// Route::delete();
+// Route::options();
+
+// Route::match(['get', 'post'], '/', function(){
+//     return 'POST and GET is allowed.';
+// });
+
+// Route::view('/welcome', 'welcome');
+
+// Route::permanentRedirect('/welcome', '/');
+
+Route::get('/', function(){
+    return 'Welcome';
+});
+
+// Route::get('/users', function(Request $request){
+//     dd($request);
+//     return null;
+// });
+
+Route::get('/user/{id}/{group}', function($id, $group){
+    return response($id.'-'.$group, 200);
+});
+
+Route::get('/request-json', function(){
+    return response()->json(['name' => 'Noriel', 'age' => '21']);
+});
+
+Route::get('/request-download', function(){
+    $path = public_path().'/robots.txt';
+    $name = 'robots.txt';
+    $headers = array(
+        'Content-type: application/text-plain',
+    );
+    return response()->download($path, $name, $headers);
+});
